8d6e08d8dd8b9ffc20587332eb464450bc581ff2
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-09-22 18:06:39 +0530

    Time: 12 ms (22.60%), Space: 9 MB (99.86%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
-        string ans;
-        sort(strs.begin(), strs.end());
-        
-        string a = strs[0];
-        string b = strs[n-1];
-        for(int i=0;i<a.size();i++)
+    string longestCommonPrefix(vector<string>& arr) {
+        int n = arr.size();
+        if (n == 1) return arr[0];
+        string res = "";
+        bool include;
+        for (int i = 0; i < arr[0].length (); ++i)
         {
-            if(a[i]==b[i])
+            include = true;
+            for (int j = 1; j < n; ++j)
             {
-                ans += a[i];
+                // checking if the current character arr[0][i] is eligible
+                // for being included in the result or not
+                if (i >= arr[j].length () || arr[0][i] != arr[j][i])
+                {
+                    include = false;
+                    break;
+                }
             }
-            else break;
+            // if it can't be included, we need not check the further characters
+            if (!include) break;
+            // if it can be included, we include it and check ahead
+            res += arr[0][i];
         }
-        return ans;
+        if (res == "") return "";
+        return res;
     }
 };
