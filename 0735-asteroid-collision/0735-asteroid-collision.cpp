ae0e3c3b270c8a46784045aa8b46bf9bfdd21934
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-10-11 00:12:03 +0530

    Time: 25 ms (56.55%), Space: 17.8 MB (40.03%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> ans;
+        stack<int> st;
+        for(auto it: asteroids)
+        {
+            if(it>0)
+            {
+                st.push(it);
+            }
+            else
+            {
+                while(!st.empty() && st.top() > 0 && st.top() < -it)
+                {
+                    st.pop();
+                }
+                if(!st.empty() && st.top() == -it)
+                {
+                    st.pop();
+                }
+                else if(!st.empty() && st.top() > -it)
+                {
+                }
+                else
+                {
+                    st.push(it);
+                }
+            }
+        }
+        while(!st.empty())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
