29108aa1927cee21c51bc0504f9f262171f69dd7
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-10-11 00:12:33 +0530

    Time: 30 ms (41.86%), Space: 17.8 MB (40.03%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -21,6 +21,7 @@ public:
                 }
                 else if(!st.empty() && st.top() > -it)
                 {
+                    continue;
                 }
                 else
                 {
