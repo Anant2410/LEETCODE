9bc041ed469760dc1a047075cbf0b23a7fb815ef
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-11-12 23:09:05 +0530

    Time: 360 ms (21.66%), Space: 59.8 MB (18.31%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& nums) {
-        int start = 0, end = nums.size()-1;
-        int n = nums.size();
-        while(start<=end){
-            int mid = start + (end-start)/2;
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int low = 0, high = arr.size()-1;
+        int n = arr.size();
+        while(low <=high)
+        {
+            int mid = low + (high-low)/2;
             
-            if(mid>0 && mid<n-1){
-                if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
+            if(mid>0 && mid<n-1)
+            {
+                if(arr[mid]>arr[mid-1] && arr[mid] > arr[mid+1])
                     return mid;
-                else if(nums[mid]<nums[mid+1])
-                    start = mid+1;
-                else 
-                    end = mid-1;
-                    
+                else if(arr[mid] < arr[mid+1])
+                    low = mid+1;
+                else
+                    high = mid-1;
             }
-            else if(mid==0){
-                if(nums[0] > nums[1])
+            else if(mid==0)
+            {
+                if(arr[mid]>arr[mid+1])
                     return 0;
                 else
                     return 1;
             }
-            else if(mid == n-1){
-                if(nums[n-1] > nums[n-2])
+            else if(mid==n-1)
+            {
+                if(arr[mid]>arr[mid-1])
                     return n-1;
                 else
                     return n-2;
