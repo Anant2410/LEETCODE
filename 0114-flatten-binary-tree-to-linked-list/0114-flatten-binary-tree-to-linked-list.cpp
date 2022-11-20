0d1923e4c1ef689beb7ac303740519e4a9ff106a
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2022-11-20 23:30:25 +0530

    Time: 7 ms (86.17%), Space: 12.9 MB (10.82%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,14 +11,27 @@
  */
 class Solution {
 public:
-    TreeNode* prev = NULL;
     void flatten(TreeNode* root) {
-        if(root==NULL)
-            return;
-        flatten(root->right);
-        flatten(root->left);
-        root->right = prev;
-        root->left = NULL;
-        prev = root;
+        if(!root) return;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty())
+        {
+            TreeNode* node = st.top();
+            st.pop();
+            if(node->right)
+            {
+                st.push(node->right);
+            }
+            if(node->left)
+            {
+                st.push(node->left);
+            }
+            if(!st.empty())
+            {
+                node->right = st.top();
+            }
+            node->left = NULL;
+        }
     }
 };
