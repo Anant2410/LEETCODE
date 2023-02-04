3987c2c268712315a2d5047b90cc8d5380805261
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-02-04 13:55:54 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,81 +3,37 @@
 using namespace std;
 
 // } Driver Code Ends
-
-
-
-class Disjoint{
-    vector<int> parent, size;
-public:
-    Disjoint(int n)
-    {
-        
-        size.resize(n+1,0);
-        parent.resize(n+1);
-        for(int i=0;i<=n;i++)
-        {
-            parent[i] = i;
-        }
-    }
-    int find_parent(int node)
-    {
-        if(node == parent[node]) return node;
-        
-        return parent[node] = find_parent(parent[node]);
-    }
-    void us(int u, int v)
-    {
-        int paru = find_parent(u);
-        int parv = find_parent(v);
-        if(size[paru] < size[parv])
-        {
-            parent[paru] = parv;
-            size[parv] += size[paru];
-        }
-        else
-        {
-            parent[parv] = paru;
-            size[paru] += size[parv];
-        }
-    }
-};
-
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> adj[])
     {
-        vector<pair<int, pair<int,int>>> edges;
-        for(int i=0;i<V;i++)
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        pq.push({0,0});
+        vector<int> vis(V,0);
+        int sum = 0;
+        while(!pq.empty())
         {
-            for(auto it: adj[i])
+            int wt = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            if(vis[node]) continue;
+            vis[node] = 1;
+            sum += wt;
+            for(auto it: adj[node])
             {
-                int node= i;
                 int adjnode = it[0];
-                int wt = it[1];
-                edges.push_back({wt,{node,adjnode}});
+                int adjwt = it[1];
+                if(!vis[adjnode])
+                {
+                    pq.push({adjwt,adjnode});
+                }
             }
         }
-        Disjoint ds(V);
-        int ans = 0;
-        sort(edges.begin(), edges.end());
-        for(auto it: edges)
-        {
-            int wt = it.first;
-            int u = it.second.first;
-            int v = it.second.second;
-            
-            if(ds.find_parent(u) != ds.find_parent(v))
-            {
-                ans += wt;
-                ds.us(u,v);
-            }
-        }
-        return ans;
+        return sum;
     }
 };
-
 
 //{ Driver Code Starts.
 
