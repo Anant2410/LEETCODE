c2c2ff88d6eb3a72227f405098a98b64d82421d2
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-02-07 19:55:05 +0530

    Added solution - LeetHub

---
+++ Geek Jump - GFG/geek-jump.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  int f(int ind, vector<int>& arr, int n, vector<int>& dp)
+  {
+      if(ind==0) return 0;
+      if(dp[ind] != -1) return dp[ind];
+      int up = f(ind-1,arr,n,dp) + abs(arr[ind] - arr[ind-1]);
+      int down = INT_MAX;
+      if(ind>1)
+      {
+        down = f(ind-2,arr,n,dp) + abs(arr[ind] - arr[ind-2]);
+      }
+      
+      return dp[ind] = min(up,down);
+  }
+    int minimumEnergy(vector<int>& height, int n) {
+        vector<int> dp(n+1,-1);
+        return f(n-1,height,n,dp);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minimumEnergy(arr, N) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
