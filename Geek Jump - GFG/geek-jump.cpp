80b14a25201214aed85ddf35817fadd6a311afb3
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-02-07 19:58:11 +0530

    Added solution - LeetHub

--- Geek Jump - GFG/geek-jump.cpp
+++ Geek Jump - GFG/geek-jump.cpp
@@ -6,22 +6,33 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int f(int ind, vector<int>& arr, int n, vector<int>& dp)
-  {
-      if(ind==0) return 0;
-      if(dp[ind] != -1) return dp[ind];
-      int up = f(ind-1,arr,n,dp) + abs(arr[ind] - arr[ind-1]);
-      int down = INT_MAX;
-      if(ind>1)
-      {
-        down = f(ind-2,arr,n,dp) + abs(arr[ind] - arr[ind-2]);
-      }
+//   int f(int ind, vector<int>& arr, int n, vector<int>& dp)
+//   {
+//       if(ind==0) return 0;
+//       if(dp[ind] != -1) return dp[ind];
+//       int up = f(ind-1,arr,n,dp) + abs(arr[ind] - arr[ind-1]);
+//       int down = INT_MAX;
+//       if(ind>1)
+//       {
+//         down = f(ind-2,arr,n,dp) + abs(arr[ind] - arr[ind-2]);
+//       }
       
-      return dp[ind] = min(up,down);
-  }
+//       return dp[ind] = min(up,down);
+//   }
     int minimumEnergy(vector<int>& height, int n) {
-        vector<int> dp(n+1,-1);
-        return f(n-1,height,n,dp);
+        vector<int> dp(n,-1);
+        dp[0] = 0;
+        for(int i=1;i<n;i++)
+        {
+            int up = dp[i-1] + abs(height[i] - height[i-1]);
+            int down = INT_MAX;
+            if(i>1)
+            {
+                down = dp[i-2] + abs(height[i] - height[i-2]);
+            }
+            dp[i] = min(up,down);
+        }
+        return dp[n-1];
     }
 };
 
