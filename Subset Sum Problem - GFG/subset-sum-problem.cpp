8d2c183f96d68f5081441b38f30193df1ad16f71
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-02-16 22:15:40 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.cpp
+++ Subset Sum Problem - GFG/subset-sum-problem.cpp
@@ -9,23 +9,31 @@ using namespace std;
 
 class Solution{   
 public:
-    bool f(int ind, int target, vector<int>& arr, vector<vector<int>>& dp)
-    {
-        if(target == 0) return true;
-        if(ind==0) return (arr[0] == target);
-        if(dp[ind][target] != -1) return dp[ind][target];
-        bool not_take = f(ind-1,target,arr,dp);
-        bool take = false;
-        if(target >= arr[ind])
-        {
-            take = f(ind-1,target-arr[ind], arr,dp);
-        }
-        return dp[ind][target] =  (take || not_take);
-    }
-    bool isSubsetSum(vector<int>arr, int sum){
+    bool isSubsetSum(vector<int>arr, int k){
         int n = arr.size();
-        vector<vector<int>> dp(n+1, vector<int>(sum+1,-1));
-        return f(n-1,sum,arr,dp);
+        vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+    
+    for(int i=0; i<n; i++){
+        dp[i][0] = true;
+    }
+    
+    if(arr[0]<=k)
+        dp[0][arr[0]] = true;
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 1; target<=k; target++){
+            
+            bool notTaken = dp[ind-1][target];
+    
+            bool taken = false;
+                if(arr[ind]<=target)
+                    taken = dp[ind-1][target-arr[ind]];
+        
+            dp[ind][target]= notTaken||taken;
+        }
+    }
+    
+    return dp[n-1][k];
     }
 };
 
