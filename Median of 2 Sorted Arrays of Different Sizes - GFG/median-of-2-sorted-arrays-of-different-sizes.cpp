3c0ee6e4a5924695791ac779a8c19462f68940ce
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-03-27 21:57:07 +0530

    Added solution - LeetHub

--- Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -13,38 +13,47 @@ class Solution{
     public:
     double MedianOfArrays(vector<int>& arr1, vector<int>& arr2)
     {
-        
+        //for declaring the smaller part array first
         if(arr2.size() < arr1.size()) return MedianOfArrays(arr2,arr1);
         
         int n1 = arr1.size();
         int n2 = arr2.size();
+        //setting high and low for the first  array
         int low = 0, high = n1;
+        //apply binary search
         while(low<=high)
         {
+            //find cut1  i.e middle
             int cut1 = low + (high-low)/2;
+            //cut2 is  the remaining left half
             int cut2 = (n1+n2+1)/2 - cut1;
-            
+            //if zero or max set to INT_MIN or INT_MAX
             int l1=cut1==0 ?INT_MIN : arr1[cut1-1];
             int l2=cut2==0 ?INT_MIN : arr2[cut2-1];
             
             int r1=cut1==n1 ?INT_MAX : arr1[cut1];
             int r2=cut2==n2 ?INT_MAX : arr2[cut2];
             
+            //if all the condition met
             if(l1<=r2 && l2 <= r1)
             {
+                //if even 
                 if((n1+n2)%2==0)
                 {
                     return (max(l1,l2) + min(r1,r2))/2.0;
                 }
+                // if  odd
                 else
                 {
                     return max(l1,l2);
                 }
             }
+            //move left
             else if(l1 > r2)
             {
                 high = cut1-1;
             }
+            //move right
             else
             {
                 low = cut1+1;
