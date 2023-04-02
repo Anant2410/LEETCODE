3ca4eea9663740ad5ed34a63c152e7f076f12fa6
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-04-02 23:43:45 +0530

    Added solution - LeetHub

---
+++ Split the binary string into substrings with equal number of 0s and 1s - GFG/split-the-binary-string-into-substrings-with-equal-number-of-0s-and-1s.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+    int maxSubStr(string str){
+        int c = 0;
+        int ans = 0;
+        for(auto it: str)
+        {
+            if(it=='0') c++;
+            if(it=='1')c--;
+            
+            if(c==0) ans++;
+        }
+        if(c) return -1;
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    string str;
+	    cin >> str;
+	    Solution ob;
+	    int ans = ob.maxSubStr(str);
+	    cout << ans<<endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
