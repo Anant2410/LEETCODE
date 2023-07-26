90d000162d882af332f657291f7152f9777a50b8
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-07-26 20:28:08 +0530

    Added solution - LeetHub

---
+++ Minimum sum partition - GFG/minimum-sum-partition.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+
+  public:
+    vector<int> subsetsum(int arr[], int n, int sum)
+    {
+        int dp[n+1][sum+1];
+        for(int i=0;i<n+1;i++)
+        {
+            for(int j=0;j<sum+1;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j] = 0;
+                }
+                if(j==0)
+                {
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        for(int i=1;i<n+1;i++)
+        {
+            for(int j=1;j<sum+1;j++)
+            {
+                if(arr[i-1] <= j)
+                {
+                    dp[i][j] = dp[i-1][j-arr[i-1]] || dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        vector<int> v;
+        for(int i=0;i<=sum/2;i++)
+        {
+            if(dp[n][i])
+            {
+                v.push_back(i);
+            }
+        }
+        return v;
+    }
+	int minDifference(int arr[], int n)  { 
+	    int sum = 0;
+	    for(int i=0;i<n;i++)
+	    {
+	        sum += arr[i];
+	    }
+	    vector<int> v = subsetsum(arr,n,sum);
+	    int ans = INT_MAX;
+	    for(int i=0;i<v.size();i++)
+	    {
+	        ans = min(ans, sum-2*v[i]);
+	    }
+	    return ans;
+	} 
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.minDifference(a, n) << "\n";
+	     
+    }
+    return 0;
+}
+// } Driver Code Ends
