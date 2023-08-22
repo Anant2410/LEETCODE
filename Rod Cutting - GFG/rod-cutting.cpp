09a57a2fa565619519fb0c9f8a07f0f45f2f8a21
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-08-22 12:38:06 +0530

    Added solution - LeetHub

--- Rod Cutting - GFG/rod-cutting.cpp
+++ Rod Cutting - GFG/rod-cutting.cpp
@@ -10,39 +10,53 @@ using namespace std;
 
 class Solution{
   public:
+    // int solve(int n, int w, int price[], int length[])
+    // {
+    //     if(n==0 || w==0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(length[n-1] <= w)
+    //     {
+    //         return max(price[n-1] + solve(n,w-length[n-1],price,length),
+    //         solve(n-1,w,price,length));
+    //     }
+    //     return solve(n-1,w,price,length);
+    // }
+    int solve(int n, int w, int price[], int length[])
+    {
+        int dp[n+1][w+1];
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0] = 0;
+        }
+        for(int j=1;j<=w;j++)
+        {
+            dp[0][j] = 0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=w;j++)
+            {
+                if(length[i-1] <= j)
+                {
+                    dp[i][j] = max(price[i-1] + dp[i][j-length[i-1]], dp[i-1][j]);
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][w];
+    }
     int cutRod(int price[], int n) {
         int length[n];
         for(int i=0;i<n;i++)
         {
             length[i] = i+1;
         }
-        int dp[n+1][n+1];
-        
-        for(int i=0;i<n+1;i++)
-        {
-           for(int j=0;j<n+1;j++)
-           {
-               if(i==0 || j==0)
-               {
-                   dp[i][j] = 0;
-               }
-           }
-        }
-        for(int i=1;i<n+1;i++)
-        {
-           for(int j=1;j<n+1;j++)
-           {
-               if(length[i-1] <= j)
-               {
-                   dp[i][j] = max(price[i-1] + dp[i][j-length[i-1]], dp[i-1][j]);
-               }
-               else
-               {
-                   dp[i][j] = dp[i-1][j];
-               }
-           }
-       }
-       return dp[n][n];
+        return solve(n,n,price,length);
     }
 };
 
