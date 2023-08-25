666b9fb7094be1a79bcf52aa664a8aefc2afc090
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-08-25 16:40:29 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -9,27 +9,89 @@ using namespace std;
 
 class Solution{
 public:
+    // int solve(string str, int i, int j, bool istrue)
+    // {
+    //     if(i>j) return false;
+    //     if(i==j)
+    //     {
+    //       if(istrue == true) return  str[i] == 'T';
+    //       else return str[i] == 'F';
+    //     }
+    //     int ans = 0;
+    //     for(int k=i+1;k<j;k+=2)
+    //     {
+    //         int lt = solve(str,i,k-1,true);
+    //         int lf = solve(str,i,k-1,false);
+    //         int rt = solve(str,k+1,j,true);
+    //         int rf = solve(str,k+1,j,false);
+            
+    //         if(str[k] =='&')
+    //         {
+    //             if(istrue)
+    //             {
+    //                 ans += lt*rt;
+    //             }
+    //             else
+    //             {
+    //                 ans += lt*rf + lf*rt + lf*rf;
+    //             }
+    //         }
+    //         else if(str[k] == '|')
+    //         {
+    //             if(istrue)
+    //             {
+    //                 ans += lt*rt + lt*rf + lf*rt;
+    //             }
+    //             else
+    //             {
+    //                 ans += lf*rf;
+    //             }
+    //         }
+    //         else if(str[k] == '^')
+    //         {
+    //             if(istrue)
+    //             {
+    //                 ans += lt*rf + lf*rt;
+    //             }
+    //             else
+    //             {
+    //                 ans += lt*rt + lf*rf;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+    // }
     int dp[201][201][2];
-    int f(string str, int i, int j, bool flag)
+    unordered_map<string,int>mp;
+    int solve(string str, int i, int j, bool istrue)
     {
         if(i>j) return 0;
         if(i==j)
         {
-            if(flag==true) return str[i] == 'T';
+            if(istrue==true) return str[i] == 'T';
             else return str[i] == 'F';
         }
-        if(dp[i][j][flag] != -1) return dp[i][j][flag];
+        //if(dp[i][j][istrue] != -1) return dp[i][j][istrue];
+        string temp  = to_string(i);
+        temp.append(" ");
+        temp.append(to_string(j));
+        temp.append(" ");
+        temp.append(to_string(istrue));
+        if(mp.count(temp))
+        {
+            return mp[temp];
+        }
         int ans = 0;
         for(int k = i+1;k<j;k+=2)
         {
-            int lt = f(str,i,k-1,true);
-            int lf = f(str,i,k-1,false);
-            int rt = f(str,k+1,j,true);
-            int rf = f(str,k+1,j,false);
+            int lt = solve(str,i,k-1,true);
+            int lf = solve(str,i,k-1,false);
+            int rt = solve(str,k+1,j,true);
+            int rf = solve(str,k+1,j,false);
             
             if(str[k] == '&')
             {
-                if(flag == true)
+                if(istrue == true)
                 {
                     ans = (ans + lt * rt)%1003 ;
                 }
@@ -40,16 +102,16 @@ public:
             }
             else if(str[k] == '|')
             {
-                if(flag == true) ans = (ans + lt*rt + lt*rf + lf*rt)%1003;
+                if(istrue == true) ans = (ans + lt*rt + lt*rf + lf*rt)%1003;
                 else ans = (ans + lf*rf)%1003;
             }
             else if(str[k] == '^')
             {
-                if(flag == true) ans = (ans + lt*rf + lf*rt)%1003;
+                if(istrue == true) ans = (ans + lt*rf + lf*rt)%1003;
                 else ans = (ans + lt*rt + rf*lf)%1003;
             }
         }
-        return dp[i][j][flag] = ans;
+        return mp[temp] = ans;
     }
     int countWays(int N, string S){
         for(int i=0;i<N+1;i++)
@@ -62,7 +124,7 @@ public:
                 }
             }
         }
-        return f(S,0,N-1,true);
+        return solve(S,0,N-1,true);
     }
 };
 
