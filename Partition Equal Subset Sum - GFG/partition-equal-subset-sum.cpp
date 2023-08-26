e926d38913d0bff8bee7fb1da71bb1f8bb01ee2e
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-08-26 22:21:48 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -9,21 +9,49 @@ using namespace std;
 
 class Solution{
 public:
-    int dp[1001][1001];
+    // int solve(int arr[], int n, int sum)
+    // {
+    //     if(n==0) return 0;
+    //     if(sum == 0) return 1;
+    //     if(dp[n][sum] != -1)
+    //     {
+    //         return dp[n][sum];
+    //     }
+        
+    //     if(arr[n-1] <= sum)
+    //     {
+    //         return dp[n][sum] = solve(arr,n-1,sum-arr[n-1]) || solve(arr,n-1,sum);
+    //     }
+    //     return dp[n][sum] =  solve(arr,n-1,sum);
+    // }
     int solve(int arr[], int n, int sum)
     {
-        if(n==0) return 0;
-        if(sum == 0) return 1;
-        if(dp[n][sum] != -1)
+        vector<int> prev(sum+1), curr(sum+1);
+        for(int i=0;i<=n;i++)
         {
-            return dp[n][sum];
+            curr[0] = 1;
+        }
+        for(int j=1;j<=sum;j++)
+        {
+            curr[j] = 0;
         }
         
-        if(arr[n-1] <= sum)
+        for(int i=1;i<=n;i++)
         {
-            return dp[n][sum] = solve(arr,n-1,sum-arr[n-1]) || solve(arr,n-1,sum);
+            for(int j=1;j<=sum;j++)
+            {
+                if(arr[i-1] <= j)
+                {
+                    curr[j] = prev[j-arr[i-1]] || prev[j];
+                }
+                else
+                {
+                    curr[j] = prev[j];
+                }
+            }
+            prev = curr;
         }
-        return dp[n][sum] =  solve(arr,n-1,sum);
+        return prev[sum];
     }
     int equalPartition(int n, int arr[])
     {
@@ -36,7 +64,6 @@ public:
         {
             return 0;
         }
-        memset(dp,-1,sizeof(dp));
         sum = sum/2;
         return solve(arr,n,sum);
     }
