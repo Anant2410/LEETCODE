0e655bfae4cee26cf98c558dbe1ba7140c378a3e
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-08-26 14:09:31 +0530

    Added solution - LeetHub

---
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -0,0 +1,107 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    // int solve(int w, int wt[], int val[], int n)
+    // {
+    //   if(n==0 || w==0) return 0;
+       
+    //   if(wt[n-1] <= w)
+    //   {
+    //       int take = val[n-1] + knapSack(w-wt[n-1],wt,val,n-1);
+    //       int nottake = knapSack(w,wt,val,n-1);
+           
+    //       return max(take,nottake);
+    //   }
+    //   else
+    //   {
+    //       return knapSack(w,wt,val,n-1);
+    //   }
+    // }
+    
+    //memoization
+    // int dp[1001][1001];
+    // int solve(int w, int wt[], int val[], int n)
+    // {
+    //   if(n==0 || w==0) return 0;
+    //   if(dp[n][w] != -1) return dp[n][w];
+    //   if(wt[n-1] <= w)
+    //   {
+    //       int take = val[n-1] + knapSack(w-wt[n-1],wt,val,n-1);
+    //       int nottake = knapSack(w,wt,val,n-1);
+           
+    //       return dp[n][w] =  max(take,nottake);
+    //   }
+    //   else
+    //   {
+    //       return dp[n][w] = knapSack(w,wt,val,n-1);
+    //   }
+    // }
+    int knapSack(int w, int wt[], int val[], int n) 
+    { 
+       //return solve(w,wt,val,n);
+       //memset(dp,-1,sizeof(dp));
+       int dp[n+1][w+1];
+       for(int i=0;i<=n;i++)
+       {
+           dp[i][0] = 0;
+       }
+       for(int j=1;j<=w;j++)
+       {
+           dp[0][j] = 0;
+       }
+       
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=w;j++)
+           {
+               if(wt[i-1] <= j)
+               {
+                   dp[i][j] = max(val[i-1] + dp[i-1][j-wt[i-1]], dp[i-1][j]);
+               }
+               else
+               {
+                   dp[i][j] = dp[i-1][j];
+               }
+           }
+       }
+       return dp[n][w];
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    //taking total testcases
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        //reading number of elements and weight
+        int n, w;
+        cin>>n>>w;
+        
+        int val[n];
+        int wt[n];
+        
+        //inserting the values
+        for(int i=0;i<n;i++)
+            cin>>val[i];
+        
+        //inserting the weights
+        for(int i=0;i<n;i++)
+            cin>>wt[i];
+        Solution ob;
+        //calling method knapSack()
+        cout<<ob.knapSack(w, wt, val, n)<<endl;
+        
+    }
+	return 0;
+}
+// } Driver Code Ends
