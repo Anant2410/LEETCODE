b1f49ee1fff1bbfe728965da06a1a60082f58f4d
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-08-29 15:59:25 +0530

    Added solution - LeetHub

--- Array Removals - GFG/array-removals.cpp
+++ Array Removals - GFG/array-removals.cpp
@@ -10,22 +10,47 @@ using namespace std;
 
 class Solution{
     public:
-    int solve(vector<int>& arr, int k, int i, int j, vector<vector<int>>& dp)
-    {
-        if(i>=j) return 0;
+    // int solve(vector<int>& arr, int k, int i, int j, vector<vector<int>>& dp)
+    // {
+    //     if(i>=j) return 0;
         
-        if(arr[j] - arr[i] <= k) return 0;
-        if(dp[i][j] != -1) return dp[i][j] ;
-        else
+    //     if(arr[j] - arr[i] <= k) return 0;
+    //     if(dp[i][j] != -1) return dp[i][j] ;
+    //     else
+    //     {
+    //         return dp[i][j] =  1 + min(solve(arr,k,i+1,j,dp), solve(arr,k,i,j-1,dp));
+    //     }
+    // }
+    int solve(vector<int>& arr, int k ,int n)
+    {
+        vector<vector<int>> dp(n, vector<int>(n,0));
+        
+        for(int i=n-1;i>=0;i--)
         {
-            return dp[i][j] =  1 + min(solve(arr,k,i+1,j,dp), solve(arr,k,i,j-1,dp));
+            for(int j=0;j<n;j++)
+            {
+                if(i>=j)
+                {
+                    dp[i][j] = 0;
+                }
+                if(arr[j] - arr[i] <= k)
+                {
+                    dp[i][j] = 0;
+                }
+                else if(arr[j] - arr[i] > k)
+                {
+                    dp[i][j] = 1 + min(dp[i+1][j], dp[i][j-1]);
+                }
+            }
         }
+        return dp[0][n-1];
     }
     int removals(vector<int>& arr, int k){
         sort(arr.begin(), arr.end());
         int n = arr.size();
-        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
-        return solve(arr,k,0,n-1,dp);
+        // vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        // return solve(arr,k,0,n-1,dp);
+        return solve(arr,k,n);
     }
 };
 
