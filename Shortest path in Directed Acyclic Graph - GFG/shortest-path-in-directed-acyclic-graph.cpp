a355658e247df2693a4caa1e1474431a9451d67a
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-09-09 14:57:05 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -8,61 +8,40 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-    void topo(int node, vector<pair<int,int>> adj[], stack<int>& st, vector<int>& vis)
-    {
-        vis[node] = 1;
-        for(auto it: adj[node])
+     vector<int> shortestPath(int n,int m, vector<vector<int>>& edges){
+        vector<int> dist(n,INT_MAX);
+        
+        vector<pair<int,int>> adj[n];
+        for(auto it: edges)
         {
-            int v = it.first;
-            if(!vis[v])
-            {
-                topo(v,adj,st,vis);
-            }
+            adj[it[0]].push_back({it[1],it[2]});
         }
-        st.push(node);
-        return ;
-    }
-    vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
-        vector<pair<int,int>> adj[N];
-        for(int i=0;i<M;i++)
-        {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int wt = edges[i][2];
-            adj[u].push_back({v,wt});
-        }
-        stack<int> st;
-        vector<int> vis(N,0);
-        for(int i=0;i<N;i++)
-        {
-            if(!vis[i])
-            {
-                topo(i,adj,st,vis);
-            }
-        }
-        vector<int> dist(N,1e9);
+        queue<int> q;
+        q.push(0);
         dist[0] = 0;
-        while(!st.empty())
+        
+        while(!q.empty())
         {
-            int node = st.top();
-            st.pop();
+            int node = q.front();
+            q.pop();
+            
             for(auto it: adj[node])
             {
-                int v = it.first;
-                int wt = it.second;
-                if(dist[v] > dist[node] + wt)
+                if(dist[it.first] > dist[node] + it.second)
                 {
-                    dist[v] = dist[node] + wt;
+                    dist[it.first] = it.second + dist[node];
+                    q.push(it.first);
                 }
             }
         }
-       for(int i=0;i<N;i++)
-       {
-           if(dist[i] == 1e9)
-           {
-               dist[i] = -1;
-           }
-       }
+        
+        for(int i=0;i<n;i++)
+        {
+            if(dist[i] == INT_MAX)
+            {
+                dist[i] = -1;
+            }
+        }
         return dist;
     }
 };
