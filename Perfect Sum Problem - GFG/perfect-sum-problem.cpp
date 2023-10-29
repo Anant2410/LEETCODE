3be6d25cb5a09b67799216afe4ba7a91cb66c19c
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-10-29 20:12:23 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -6,50 +6,33 @@ using namespace std;
 class Solution{
 
 	public:
-// 	int subsetSum(int arr[], int sum, int n)
-// 	{
-// 	    if(n==0)
-//         {
-//             return arr[0] == sum;
-//         }
-//         if(sum==0)
-//         {
-//             return 1;
-//         }
-        
-//         if(arr[n-1] <= sum)
-//         {
-//             return perfectSum(arr,n-1,sum-arr[n-1]) + perfectSum(arr,n-1,sum);
-//         }
-//         return perfectSum(arr,n-1,sum);
-// 	}
 	int perfectSum(int arr[], int n, int sum)
 	{
-        long long m=1e9+7;
+	    long long m=1e9+7;
         int dp[n+1][sum+1];
-        
-        for(int i=0; i<n+1; i++)
+        for(int i=0;i<=n;i++)
         {
-            for(int j=0; j<sum+1; j++)
-            {
-                if(i==0)dp[i][j]=0;
-                if(j==0)dp[i][j]=1;
-            }
+            dp[i][0] = 1;
         }
-        for(int i=1; i<n+1; i++)
+        for(int j=1;j<=sum;j++)
         {
-            for(int j=0; j<sum+1; j++)
+            dp[0][j] = 0;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<=sum;j++)
             {
-                if(arr[i-1]<=j)
+                if(arr[i-1] <= j)
                 {
-                    dp[i][j]=dp[i-1][j-arr[i-1]]%m+dp[i-1][j]%m;
+                    int take = dp[i-1][j-arr[i-1]]%m;
+                    int notake = dp[i-1][j]%m;
+                    dp[i][j] = (take + notake)%m;
                 }
                 else
                 {
-                    dp[i][j]=dp[i-1][j]%m;
+                    dp[i][j] = dp[i-1][j]%m;
                 }
             }
-            
         }
         return dp[n][sum]%m;
 	}
