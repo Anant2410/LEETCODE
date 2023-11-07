59dcc36b44904075234fc818e97325df16c06235
Anant2410 <74268594+Anant2410@users.noreply.github.com>
2023-11-07 18:02:57 +0530

    Added solution - LeetHub

--- Boolean Parenthesization - GFG/boolean-parenthesization.cpp
+++ Boolean Parenthesization - GFG/boolean-parenthesization.cpp
@@ -9,59 +9,6 @@ using namespace std;
 
 class Solution{
 public:
-    // int solve(string str, int i, int j, bool istrue)
-    // {
-    //     if(i>j) return false;
-    //     if(i==j)
-    //     {
-    //       if(istrue == true) return  str[i] == 'T';
-    //       else return str[i] == 'F';
-    //     }
-    //     int ans = 0;
-    //     for(int k=i+1;k<j;k+=2)
-    //     {
-    //         int lt = solve(str,i,k-1,true);
-    //         int lf = solve(str,i,k-1,false);
-    //         int rt = solve(str,k+1,j,true);
-    //         int rf = solve(str,k+1,j,false);
-            
-    //         if(str[k] =='&')
-    //         {
-    //             if(istrue)
-    //             {
-    //                 ans += lt*rt;
-    //             }
-    //             else
-    //             {
-    //                 ans += lt*rf + lf*rt + lf*rf;
-    //             }
-    //         }
-    //         else if(str[k] == '|')
-    //         {
-    //             if(istrue)
-    //             {
-    //                 ans += lt*rt + lt*rf + lf*rt;
-    //             }
-    //             else
-    //             {
-    //                 ans += lf*rf;
-    //             }
-    //         }
-    //         else if(str[k] == '^')
-    //         {
-    //             if(istrue)
-    //             {
-    //                 ans += lt*rf + lf*rt;
-    //             }
-    //             else
-    //             {
-    //                 ans += lt*rt + lf*rf;
-    //             }
-    //         }
-    //     }
-    //     return ans;
-    // }
-    int dp[201][201][2];
     unordered_map<string,int>mp;
     int solve(string str, int i, int j, bool istrue)
     {
@@ -114,19 +61,10 @@ public:
         return mp[temp] = ans;
     }
     int countWays(int N, string S){
-        for(int i=0;i<N+1;i++)
-        {
-            for(int j=0;j<N+1;j++)
-            {
-                for(int k=0;k<2;k++)
-                {
-                    dp[i][j][k] = -1;
-                }
-            }
-        }
         return solve(S,0,N-1,true);
     }
 };
+
 
 //{ Driver Code Starts.
 
